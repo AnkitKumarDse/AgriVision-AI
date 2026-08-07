10bde02d3da1ddf667d98e94295bbd9e778df1c8
AnkitKumarDse <ak1132883@gmail.com>
2026-08-08 00:13:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -668,9 +668,9 @@ with tabs[3]:
 
             crop_encoded = crop_encoder.transform([crop])[0]
             st.write("Season selected:", repr(season))
-st.write("Encoder classes:", season_encoder.classes_)
+            st.write("Encoder classes:", season_encoder.classes_)
 
-season_encoded = season_encoder.transform([season])[0]
+            season_encoded = season_encoder.transform([season])[0]
             state_encoded = state_encoder.transform([state])[0]
 
             input_data = pd.DataFrame(
